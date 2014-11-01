01f48938fc67e781d3af05426282d71d2a43a678
Samuli Thomasson <simsaladin@paivola.fi>
2014-11-01 17:34:36 +0200

    Value basics

--- src/Mahjong/Hand/Value.hs
+++ src/Mahjong/Hand/Value.hs
@@ -19,9 +19,19 @@ import Mahjong.Hand
 import Mahjong.Hand.Mentsu
 import Mahjong.Tiles
 
+getValue :: ValueInfo -> Value
+getValue = Value <$> getYaku <*> getFu
+
+getYaku :: ValueInfo -> [Yaku]
+getYaku = undefined
+
 -- | Calculate fu points for the mentsu.
-getFu :: [Mentsu] -> Fu
-getFu = sum . map mentsuValue
+getFu :: ValueInfo -> Fu
+getFu = (+) <$> sum . map mentsuValue . vMentsu
+            <*> waitValue
+
+waitValue :: ValueInfo -> Fu
+waitValue = undefined
 
 mentsuValue :: Mentsu -> Fu
 mentsuValue (Mentsu mk t ms) = product [gokind mk, gotile, goshout ms]
